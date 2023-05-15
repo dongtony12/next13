ce0622f1350804d935d50c1694268ee6dd8525cb
dongtony12 <dongtony0523@naver.com>
2023-05-16 07:43:21 +0900

    layout과 page 테스트

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="border-2 border-red-400 ">dashboard layout{children}</div>
+  )
+}
+
+export default DashboardLayout
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <div className="border-2 border-orange-300">Root Page</div>;
+  return (
+    <div className="border-2 border-orange-300">
+      Root Page
+      <Link href={'/dashboard'}>
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+          go to dashboard
+        </button>
+      </Link>
+    </div>
+  )
 }
